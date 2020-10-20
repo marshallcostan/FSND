cba6311f2d5213742fda71ac354d7d636f65c543
Marshall Costan <marshallcostan@gmail.com>
2020-10-20 16:39:49 -0400

    first commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-#export DATABASE_URL=postgres://fyhqipyxocepsi:5c064d64c3a0ce4e13c923a3c0b4a33ba3559a838dcf4059aeef958a410c6d65@ec2-52-207-25-133.compute-1.amazonaws.com:5432/d3ormi4qtrldae
-#export EXCITED=true
+export DATABASE_URL=postgres://fyhqipyxocepsi:5c064d64c3a0ce4e13c923a3c0b4a33ba3559a838dcf4059aeef958a410c6d65@ec2-52-207-25-133.compute-1.amazonaws.com:5432/d3ormi4qtrldae
+export EXCITED=true
